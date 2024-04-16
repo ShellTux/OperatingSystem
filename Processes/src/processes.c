cf89e2b2bc6969873e64706d42197d69e8ee0ac2
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-04-16 19:38:14 +0100

    chore(processes.c): add license header

--- Processes/src/processes.c
+++ Processes/src/processes.c
@@ -1,3 +1,25 @@
+/*******************************************************************************
+ * Project    ___                     _   _           ___         _
+ *           / _ \ _ __  ___ _ _ __ _| |_(_)_ _  __ _/ __|_  _ __| |_ ___ _ __
+ *          | (_) | '_ \/ -_) '_/ _` |  _| | ' \/ _` \__ \ || (_-<  _/ -_) '  \
+ *           \___/| .__/\___|_| \__,_|\__|_|_||_\__, |___/\_, /__/\__\___|_|_|_|
+ *                |_|                           |___/     |__/
+ *
+ *
+ * Author: Luís Góis
+ *
+ * This software is licensed as described in the file LICENSE, which
+ * you should have received as part of this distribution.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the LICENSE file.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ***************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
